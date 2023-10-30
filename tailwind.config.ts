fff4b96f2c99c29fa64bef2d6480435e498129ab
mbartos <maciekbartos8@gmail.com>
2023-10-30 22:39:07 +0100

    Fix theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,14 @@ const config: Config = {
 			lato: ['Lato', 'sans-serif'],
 			jost: ['Jost', 'sans-serif'],
 		},
-		colors: {
-			primary: '#272727',
-			accent: '#3A3A3A',
-			hero: '#EDF1F3',
-			secondary: '#72aec8',
-			gray: '#848484',
+		extend: {
+			colors: {
+				primary: '#272727',
+				accent: '#3A3A3A',
+				hero: '#EDF1F3',
+				secondary: '#72aec8',
+				gray: '#848484',
+			},
 		},
 	},
 	plugins: [],
