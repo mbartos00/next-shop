58f336f74162ab7af1b544bfbaab00fc3f0671e6
mbartos <maciekbartos8@gmail.com>
2023-11-01 16:06:30 +0100

    Add copyright section

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,11 +1,15 @@
+import Copyrights from './Copyrights';
 import InstagramGallery from './InstagramGallery';
 import Newsletter from './Newsletter';
 
 const Footer = () => {
 	return (
-		<footer className='flex flex-col md:justify-center xl:px-[15vw]'>
-			<Newsletter />
-			<InstagramGallery />
+		<footer className='flex flex-col md:justify-center '>
+			<div className='xl:px-[15vw]'>
+				<Newsletter />
+				<InstagramGallery />
+			</div>
+			<Copyrights />
 		</footer>
 	);
 };
