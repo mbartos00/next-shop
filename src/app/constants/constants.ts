8a29cca4c6ac5d7692d0df6ed47a57fda2c98eb8
mbartos <maciekbartos8@gmail.com>
2023-11-01 17:19:43 +0100

    Extract links

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,40 @@
+export const navLinks = [
+	{
+		title: 'Home',
+		path: '/',
+	},
+	{
+		title: 'Shop',
+		path: '/shop',
+	},
+	{
+		title: 'About',
+		path: '/about',
+	},
+	{
+		title: 'Blogs',
+		path: '/blogs',
+	},
+	{
+		title: 'Contact',
+		path: '/contact',
+	},
+];
+export const footerLinks = [
+	{
+		title: 'TRACK YOUR ORDER',
+		path: '/tracking',
+	},
+	{
+		title: 'RETURNS POLICY',
+		path: '/returns',
+	},
+	{
+		title: 'SHIPPING + DELIVERY',
+		path: '/shipping',
+	},
+	{
+		title: 'FAQS',
+		path: '/faq',
+	},
+];
