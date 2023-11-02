3bded702345c0f15f6fc1d866e2266df3191d0d0
mbartos <maciekbartos8@gmail.com>
2023-11-02 17:43:15 +0100

    Add breadcrumbs

---
+++ src/app/components/Hero/Breadcrumbs.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import capitalize from '@/app/utils/capitalize';
+
+const Breadcrumbs = () => {
+	const pathname = usePathname();
+	const isHome = pathname === '/';
+	const location = pathname.slice(1, pathname.length);
+	const locationName = capitalize(location);
+	return (
+		<>
+			{!isHome && (
+				<div>
+					<h3 className='text-base font-light text-primary'>
+						Home {'>'} <span className='text-secondary underline'>{locationName}</span>
+					</h3>
+				</div>
+			)}
+		</>
+	);
+};
+
+export default Breadcrumbs;
