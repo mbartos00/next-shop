add8401550651c1472d8b3bb567f6bdd3584c6a3
mbartos <maciekbartos8@gmail.com>
2023-11-03 16:25:49 +0100

    fix styles

--- src/app/components/Footer/Informations.tsx
+++ src/app/components/Footer/Informations.tsx
@@ -17,7 +17,7 @@ const Informations = () => {
 		<div className='mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 md:px-4 xl:mt-24 xl:grid-cols-[2fr_repeat(3,_1fr)] xl:gap-6 xl:px-0'>
 			<section className='flex flex-col items-center text-primary xl:items-start '>
 				<h2 className='mb-2.5 text-4xl font-medium'>NextShop</h2>
-				<p className='w-80 text-center text-base font-light xl:w-96 xl:text-left  '>
+				<p className='w-[80vw] max-w-xs text-center text-base font-light md:w-80 md:max-w-none xl:w-96 xl:text-left  '>
 					Nisi, purus vitae, ultrices nunc. Sit ac sit suscipit hendrerit. Gravida massa volutpat
 					aenean odio erat nullam fringilla.
 				</p>
@@ -37,7 +37,7 @@ const Informations = () => {
 			</section>
 			<InformationsLinksSection text='quick links' links={navLinks} />
 			<InformationsLinksSection text='help & info' links={footerLinks} />
-			<section className='flex flex-col items-center text-primary xl:items-start'>
+			<section className='flex flex-col items-center text-center text-primary md:text-left xl:items-start'>
 				<h3 className='text-xl font-medium '>CONTACT US</h3>
 				<p className='font-light'>Do you have any queries or suggestions?</p>
 				<p className='font-medium underline'>yourinfo@gmail.com</p>
