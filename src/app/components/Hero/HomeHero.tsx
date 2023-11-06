51d795161a6b4c7f45d74ff8ea9f3ebf9e16b5cc
mbartos <maciekbartos8@gmail.com>
2023-11-06 17:49:59 +0100

    Add mocked content

--- src/app/components/Hero/HomeHero.tsx
+++ src/app/components/Hero/HomeHero.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Carousel from '../Carousel';
 
+const mockedContent = [
+	{
+		id: Math.floor(Math.random() * 100),
+		image: '/heroImage.png',
+	},
+	{
+		id: Math.floor(Math.random() * 100),
+		image: '/hero2.png',
+	},
+];
+
 const HomeHero = () => {
 	return (
 		<div>
-			<Carousel
-				variant='hero'
-				heading='your products are great'
-				arrows
-				content={['/heroImage.png', '/hero2.png']}
-			/>
+			<Carousel variant='hero' heading='your products are great' arrows content={mockedContent} />
 		</div>
 	);
 };
