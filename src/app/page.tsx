6cbaf0a74864848eb73a9aa51115269cc74cd5c5
mbartos <maciekbartos8@gmail.com>
2023-11-08 14:29:59 +0100

    Add product section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
 import FeatureList from './components/FeatureList';
 import HomeHero from './components/Hero/HomeHero';
+import ProductSection from './components/ProductSection';
+
+const mockedContent = [
+	{
+		id: 1,
+		title: 'Iphone',
+		price: 1234,
+		image: '/mockImages/phone.jpg',
+	},
+	{
+		id: 2,
+		title: 'Iphone2',
+		price: 1234,
+		image: '/mockImages/phone.png',
+	},
+	{
+		id: 3,
+		title: 'Iphone3',
+		price: 1234,
+		image: '/mockImages/phone.png',
+	},
+	{
+		id: 4,
+		title: 'Iphone4',
+		price: 1234,
+		image: '/mockImages/phone.png',
+	},
+	{
+		id: 5,
+		title: 'Iphone5',
+		price: 1234,
+		image: '/mockImages/phone.png',
+	},
+	{
+		id: 6,
+		title: 'Iphone6',
+		price: 1234,
+		image: '/mockImages/phone.png',
+	},
+];
 
 const Home = () => {
 	return (
@@ -7,6 +47,8 @@ const Home = () => {
 			<HomeHero />
 			<div className='xl:px-[15vw]'>
 				<FeatureList />
+				<ProductSection content={mockedContent} heading='mobile products' />
+				<ProductSection content={mockedContent} heading='smart watches' />
 			</div>
 		</main>
 	);
