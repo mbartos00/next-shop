f4cfc7223bc44ef79114bb49b298434f0bdd85b3
mbartos <maciekbartos8@gmail.com>
2023-11-08 21:25:32 +0100

    Add new variant in carousel

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -6,20 +6,33 @@ import { useEffect, useState } from 'react';
 import Swipe from 'react-easy-swipe';
 
 type Props = {
-	variant: 'hero' | 'slider';
-	content: any[];
+	variant: 'hero' | 'review';
 	arrows?: boolean;
-	heading?: string;
 } & (
 	| {
 			variant: 'hero';
 			heading: string;
+			content: Content[];
 	  }
 	| {
-			variant: 'slider';
+			variant: 'review';
 			heading?: string;
+			content: Review[];
 	  }
 );
+
+export type Content = {
+	id: string | number;
+	title?: string;
+	price?: number;
+	image: string;
+};
+type Review = {
+	id: string | number;
+	review: string;
+	rating: number;
+	createdBy: string;
+};
 
 const Carousel = ({ content, variant, arrows, heading }: Props) => {
 	const [currentSlide, setCurrentSlide] = useState(0);
@@ -46,7 +59,7 @@ const Carousel = ({ content, variant, arrows, heading }: Props) => {
 	};
 
 	return (
-		<div className='relative z-0 bg-[#EDF1F3]'>
+		<div className={`relative z-0 ${variant === 'review' ? 'bg-white' : 'bg-[#EDF1F3]'}`}>
 			{arrows && (
 				<Image
 					src={'/heroArrow.svg'}
@@ -55,7 +68,11 @@ const Carousel = ({ content, variant, arrows, heading }: Props) => {
 					width={0}
 					height={0}
 					onClick={handlePrevSlide}
-					className='absolute inset-y-1/2 left-2 z-20 m-auto h-[15vw] cursor-pointer md:h-[8vw] xl:max-h-[120px]'
+					className={`absolute inset-y-1/2 left-2 z-20 m-auto ${
+						variant === 'hero'
+							? 'h-[15vw] md:h-[8vw] xl:max-h-[120px]'
+							: 'h-[8vw] xl:h-[5vw] 2xl:h-[3vw]'
+					} cursor-pointer`}
 				/>
 			)}
 			<div className='relative m-auto flex w-full overflow-hidden'>
@@ -67,9 +84,9 @@ const Carousel = ({ content, variant, arrows, heading }: Props) => {
 						onSwipeRight={handlePrevSlide}
 						className='relative z-10 h-full w-full'
 					>
-						{content.map((image, index) => {
-							if (index === currentSlide) {
-								if (variant === 'hero') {
+						{variant === 'hero' &&
+							content.map((item, index) => {
+								if (index === currentSlide) {
 									return (
 										<motion.div
 											key={index}
@@ -81,8 +98,8 @@ const Carousel = ({ content, variant, arrows, heading }: Props) => {
 											className='grid grid-cols-1 place-items-center text-center uppercase text-primary xl:grid-cols-4 xl:py-8'
 										>
 											<div className='mb-4 xl:col-start-2 xl:mb-0 xl:text-left'>
-												<h1 className='mb-8 mt-8 text-4xl font-light md:text-5xl xl:mb-10 xl:mt-0 xl:text-6xl'>
-													{heading?.toUpperCase()}
+												<h1 className='mb-8 mt-8 text-4xl font-light uppercase md:text-5xl xl:mb-10 xl:mt-0 xl:text-6xl'>
+													{heading}
 												</h1>
 												<Link
 													className='mt-6 bg-primary px-6 py-2 text-sm font-medium tracking-wider text-white xl:py-3'
@@ -92,8 +109,8 @@ const Carousel = ({ content, variant, arrows, heading }: Props) => {
 												</Link>
 											</div>
 											<Image
-												key={image.id}
-												src={image}
+												key={item.id}
+												src={item.image}
 												style={{ maxWidth: '530px', maxHeight: '600px' }}
 												width={530}
 												height={0}
@@ -103,8 +120,56 @@ const Carousel = ({ content, variant, arrows, heading }: Props) => {
 										</motion.div>
 									);
 								}
-							}
-						})}
+							})}
+						{variant === 'review' &&
+							content.map((item, index) => {
+								if (index === currentSlide) {
+									return (
+										<motion.div
+											key={index}
+											variants={variants}
+											custom={currentSlide}
+											initial={'enter'}
+											animate={'center'}
+											exit={'exit'}
+											className='grid grid-cols-1 place-items-center text-center uppercase text-primary'
+										>
+											<div className='mb-4 flex flex-col items-center gap-4 xl:col-start-2 xl:mb-14 '>
+												<Image
+													src={'/quoteIcon.svg'}
+													alt='quote icon'
+													width={50}
+													height={50}
+													style={{
+														width: '100%',
+														height: '100%',
+														maxWidth: '60px',
+														maxHeight: '60px',
+													}}
+												/>
+												<q className=' mb-4 block w-[70%] text-base font-light md:text-lg xl:mt-0 xl:text-2xl'>
+													{`${item.review}`}
+												</q>
+												<span className='flex gap-2'>
+													{Array.from({ length: 5 }).map((_, idx) => (
+														<Image
+															key={idx}
+															src={'/starIcon.svg'}
+															alt='star rating icon'
+															width={15}
+															height={15}
+															className={`${
+																idx < item.rating ? '' : 'opacity-70 contrast-200 grayscale'
+															}`}
+														/>
+													))}
+												</span>
+												<p className='font-semibold'>{item.createdBy}</p>
+											</div>
+										</motion.div>
+									);
+								}
+							})}
 					</Swipe>
 				</AnimatePresence>
 			</div>
@@ -116,28 +181,12 @@ const Carousel = ({ content, variant, arrows, heading }: Props) => {
 					width={0}
 					height={0}
 					onClick={handleNextSlide}
-					className='absolute inset-y-1/2 right-2 z-20 m-auto h-[15vw] rotate-180 cursor-pointer md:h-[8vw] xl:max-h-[125px]'
+					className={`absolute inset-y-1/2 right-2 z-20 m-auto ${
+						variant === 'hero'
+							? 'h-[15vw] md:h-[8vw] xl:max-h-[125px]'
+							: 'h-[8vw] xl:h-[5vw] 2xl:h-[3vw]'
+					} rotate-180 cursor-pointer`}
 				/>
-			)}
-
-			{variant !== 'hero' && (
-				<div className='relative flex justify-center p-2'>
-					{content.map((_, index) => {
-						return (
-							<div
-								className={
-									index === currentSlide
-										? 'mx-1 mb-1 mt-3 h-2 w-2 cursor-pointer rounded-full bg-primary'
-										: 'mx-1 mb-1 mt-3 h-2 w-2 cursor-pointer rounded-full bg-secondary'
-								}
-								key={index}
-								onClick={() => {
-									setCurrentSlide(index);
-								}}
-							/>
-						);
-					})}
-				</div>
 			)}
 		</div>
 	);
