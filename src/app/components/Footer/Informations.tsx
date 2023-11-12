9886621414148a6975cf695e63b18ae002fae1e5
mbartos <maciekbartos8@gmail.com>
2023-11-12 11:35:35 +0100

    Change content

--- src/app/components/Footer/Informations.tsx
+++ src/app/components/Footer/Informations.tsx
@@ -5,47 +5,48 @@ import React from 'react';
 import InformationsLinksSection from './InformationsLinksSection';
 
 const socialMediaIcons = [
-	'/facebookIcon.svg',
-	'/instagramIcon.svg',
-	'/twitterIcon.svg',
-	'/linkedinIcon.svg',
-	'/youtubeIcon.svg',
+  '/facebookIcon.svg',
+  '/instagramIcon.svg',
+  '/twitterIcon.svg',
+  '/linkedinIcon.svg',
+  '/youtubeIcon.svg',
 ];
 
 const Informations = () => {
-	return (
-		<div className='mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 md:px-4 xl:mt-24 xl:grid-cols-[2fr_repeat(3,_1fr)] xl:gap-6 xl:px-0'>
-			<section className='flex flex-col items-center text-primary xl:items-start '>
-				<h2 className='mb-2.5 text-4xl font-medium'>NextShop</h2>
-				<p className='w-[80vw] max-w-xs text-center text-base font-light md:w-80 md:max-w-none xl:w-96 xl:text-left  '>
-					Nisi, purus vitae, ultrices nunc. Sit ac sit suscipit hendrerit. Gravida massa volutpat
-					aenean odio erat nullam fringilla.
-				</p>
-				<div className='mt-5 flex gap-6'>
-					{socialMediaIcons.map((icon) => (
-						<Link href={'#'} key={icon}>
-							<Image
-								src={icon}
-								alt='social media icon'
-								style={{ width: '25px', aspectRatio: 1 }}
-								width={0}
-								height={0}
-							/>
-						</Link>
-					))}
-				</div>
-			</section>
-			<InformationsLinksSection text='quick links' links={navLinks} />
-			<InformationsLinksSection text='help & info' links={footerLinks} />
-			<section className='flex flex-col items-center text-center text-primary md:text-left xl:items-start'>
-				<h3 className='text-xl font-medium '>CONTACT US</h3>
-				<p className='font-light'>Do you have any queries or suggestions?</p>
-				<p className='font-medium underline'>yourinfo@gmail.com</p>
-				<p className='font-light'>If you need support? Just give us a call.</p>
-				<p className='font-medium underline'>+55 111 222 333 44</p>
-			</section>
-		</div>
-	);
+  return (
+    <div className='mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 md:px-4 xl:mt-24 xl:grid-cols-[2fr_repeat(3,_1fr)] xl:gap-6 xl:px-0'>
+      <section className='flex flex-col items-center text-primary xl:items-start '>
+        <h2 className='mb-2.5 text-4xl font-medium'>NextShop</h2>
+        <p className='w-[80vw] max-w-xs text-center text-base font-light md:w-80 md:max-w-none xl:w-96 xl:text-left  '>
+          Immerse yourself in innovation at NextShop. Discover a curated collection of
+          high-performance phones, stylish smartwatches, powerful laptops and more. Elevate your
+          tech lifestyle with the latest in electronics.
+        </p>
+        <div className='mt-5 flex gap-6'>
+          {socialMediaIcons.map((icon) => (
+            <Link href={'#'} key={icon}>
+              <Image
+                src={icon}
+                alt='social media icon'
+                style={{ width: '25px', aspectRatio: 1 }}
+                width={0}
+                height={0}
+              />
+            </Link>
+          ))}
+        </div>
+      </section>
+      <InformationsLinksSection text='quick links' links={navLinks} />
+      <InformationsLinksSection text='help & info' links={footerLinks} />
+      <section className='flex flex-col items-center text-center text-primary md:text-left xl:items-start'>
+        <h3 className='text-xl font-medium '>CONTACT US</h3>
+        <p className='font-light'>Do you have any queries or suggestions?</p>
+        <p className='font-medium underline'>yourinfo@gmail.com</p>
+        <p className='font-light'>If you need support? Just give us a call.</p>
+        <p className='font-medium underline'>+55 111 222 333 44</p>
+      </section>
+    </div>
+  );
 };
 
 export default Informations;
--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,62 +1,65 @@
 export const navLinks = [
-	{
-		title: 'Home',
-		path: '/',
-	},
-	{
-		title: 'Shop',
-		path: '/shop',
-	},
-	{
-		title: 'About',
-		path: '/about',
-	},
-	{
-		title: 'Blogs',
-		path: '/blogs',
-	},
-	{
-		title: 'Contact',
-		path: '/contact',
-	},
+  {
+    title: 'Home',
+    path: '/',
+  },
+  {
+    title: 'Shop',
+    path: '/shop',
+  },
+  {
+    title: 'About',
+    path: '/about',
+  },
+  {
+    title: 'Blogs',
+    path: '/blogs',
+  },
+  {
+    title: 'Contact',
+    path: '/contact',
+  },
 ];
 export const footerLinks = [
-	{
-		title: 'TRACK YOUR ORDER',
-		path: '/tracking',
-	},
-	{
-		title: 'RETURNS POLICY',
-		path: '/returns',
-	},
-	{
-		title: 'SHIPPING + DELIVERY',
-		path: '/shipping',
-	},
-	{
-		title: 'FAQS',
-		path: '/faq',
-	},
+  {
+    title: 'TRACK YOUR ORDER',
+    path: '/tracking',
+  },
+  {
+    title: 'RETURNS POLICY',
+    path: '/returns',
+  },
+  {
+    title: 'SHIPPING + DELIVERY',
+    path: '/shipping',
+  },
+  {
+    title: 'FAQS',
+    path: '/faq',
+  },
 ];
 export const featureList = [
-	{
-		icon: '/featureCartIcon.svg',
-		title: 'free delivery',
-		description: 'Consectetur adipi elit lorem ipsum dolor sit amet.',
-	},
-	{
-		icon: '/featureQualityIcon.svg',
-		title: 'quality guarantee',
-		description: 'Consectetur adipi elit lorem ipsum dolor sit amet.',
-	},
-	{
-		icon: '/featureOffersIcon.svg',
-		title: 'daily offers',
-		description: 'Consectetur adipi elit lorem ipsum dolor sit amet.',
-	},
-	{
-		icon: '/featureGuardIcon.svg',
-		title: '100% secure payment',
-		description: 'Consectetur adipi elit lorem ipsum dolor sit amet.',
-	},
+  {
+    icon: '/featureCartIcon.svg',
+    title: 'free delivery',
+    description:
+      'Get free delivery on all orders, so you can get your products quickly and conveniently.',
+  },
+  {
+    icon: '/featureQualityIcon.svg',
+    title: 'quality guarantee',
+    description: 'We guarantee the quality of all our products, so you can shop with confidence.',
+  },
+  {
+    icon: '/featureOffersIcon.svg',
+    title: 'daily offers',
+    description:
+      'Take advantage of our daily offers to save even more on your favorite electronics.',
+  },
+  {
+    icon: '/featureGuardIcon.svg',
+    title: '100% secure payment',
+    description:
+      'Enjoy secure payments with the latest encryption technology, so you can shop with peace of mind.',
+  },
 ];
