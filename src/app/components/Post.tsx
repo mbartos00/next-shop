b3702916eef4f917276383f16e260f5f78a3ea87
mbartos <maciekbartos8@gmail.com>
2023-11-12 11:22:23 +0100

    Add posts from api

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -1,35 +1,32 @@
 import Image from 'next/image';
 import React from 'react';
+import { PostType } from '../types/types';
+import dayjs from 'dayjs';
+import Link from 'next/link';
 
-export type Post = {
-	id: string | number;
-	image: string;
-	category: string;
-	title: string;
-	createdAt: string;
-};
-
-const Post = ({ image, category, title, createdAt }: Post) => {
-	return (
-		<div className='mb-4 flex w-full flex-col items-center justify-center px-6 md:mb-0 xl:px-0'>
-			<div className='w-full'>
-				<Image
-					src={image}
-					quality={100}
-					width={420}
-					height={0}
-					style={{ width: '100%', aspectRatio: 4 / 3 }}
-					alt={`${title} post image`}
-				/>
-				<div className='mt-4 uppercase'>
-					<p className='text-xs font-medium text-[#848484]'>
-						{createdAt} - {category}
-					</p>
-					<h3 className='text-sm font-medium text-primary'>{title}</h3>
-				</div>
-			</div>
-		</div>
-	);
+const Post = ({ id, image, category, title, createdAt }: Partial<PostType>) => {
+  return (
+    <div className='mb-4 flex w-full flex-col items-center justify-center px-6 md:mb-0 xl:px-0'>
+      <div className='w-full'>
+        <Link href={`/blogs/${id}`}>
+          <Image
+            src={image![0].path}
+            quality={100}
+            width={420}
+            height={0}
+            style={{ width: '100%', aspectRatio: 4 / 3 }}
+            alt={`${title} post image`}
+          />
+        </Link>
+        <div className='mt-4 uppercase'>
+          <p className='text-xs font-medium text-[#848484]'>
+            {dayjs(createdAt).format('MMM DD, YYYY')} - {category?.name}
+          </p>
+          <h3 className='text-sm font-medium text-primary'>{title}</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,115 +1,89 @@
 import Carousel from './components/Carousel';
 import FeatureList from './components/FeatureList';
 import HomeHero from './components/Hero/HomeHero';
-import Post from './components/Post';
 import Posts from './components/Posts';
 import ProductSection from './components/ProductSection';
 import Sales from './components/Sales';
 
 const mockedContent = [
-	{
-		id: 1,
-		title: 'Iphone',
-		price: 1234,
-		image: '/mockImages/phone.jpg',
-	},
-	{
-		id: 2,
-		title: 'Iphone2',
-		price: 1234,
-		image: '/mockImages/phone.png',
-	},
-	{
-		id: 3,
-		title: 'Iphone3',
-		price: 1234,
-		image: '/mockImages/phone.png',
-	},
-	{
-		id: 4,
-		title: 'Iphone4',
-		price: 1234,
-		image: '/mockImages/phone.png',
-	},
-	{
-		id: 5,
-		title: 'Iphone5',
-		price: 1234,
-		image: '/mockImages/phone.png',
-	},
-	{
-		id: 6,
-		title: 'Iphone6',
-		price: 1234,
-		image: '/mockImages/phone.png',
-	},
+  {
+    id: 1,
+    title: 'Iphone',
+    price: 1234,
+    image: '/mockImages/phone.jpg',
+  },
+  {
+    id: 2,
+    title: 'Iphone2',
+    price: 1234,
+    image: '/mockImages/phone.png',
+  },
+  {
+    id: 3,
+    title: 'Iphone3',
+    price: 1234,
+    image: '/mockImages/phone.png',
+  },
+  {
+    id: 4,
+    title: 'Iphone4',
+    price: 1234,
+    image: '/mockImages/phone.png',
+  },
+  {
+    id: 5,
+    title: 'Iphone5',
+    price: 1234,
+    image: '/mockImages/phone.png',
+  },
+  {
+    id: 6,
+    title: 'Iphone6',
+    price: 1234,
+    image: '/mockImages/phone.png',
+  },
 ];
 
-const mockedPosts = [
-	{
-		id: 1,
-		image: '/mockImages/postImage.png',
-		category: 'Gadgets',
-		title: 'Get some cool gadgets in 2023',
-		createdAt: 'feb 22, 2023',
-	},
-
-	{
-		id: 2,
-		image: '/mockImages/postImage.png',
-		category: 'Gadgets',
-		title: 'Get some cool gadgets in 2023',
-		createdAt: 'feb 22, 2023',
-	},
-
-	{
-		id: 3,
-		image: '/mockImages/postImage.png',
-		category: 'Gadgets',
-		title: 'Get some cool gadgets in 2023',
-		createdAt: 'feb 22, 2023',
-	},
-];
 const mockedReviews = [
-	{
-		id: 1,
-		review:
-			'Tempus oncu enim pellen tesque este pretium in neque, elit morbi sagittis lorem habi mattis Pellen tesque pretium feugiat vel morbi suspen dise sagittis lorem habi tasse morbi.',
-		rating: 4,
-		createdBy: 'John Doe',
-	},
-	{
-		id: 2,
-		review:
-			'Tempus oncu enim pellen tesque este pretium in neque, elit morbi sagittis lorem habi mattis Pellen tesque pretium feugiat vel morbi suspen dise sagittis lorem habi tasse morbi.',
-		rating: 5,
-		createdBy: 'Emma Doe',
-	},
-	{
-		id: 3,
-		review:
-			'Tempus oncu enim pellen tesque este pretium in neque, elit morbi sagittis lorem habi mattis Pellen tesque pretium feugiat vel morbi suspen dise sagittis lorem habi tasse morbi.',
-		rating: 2,
-		createdBy: 'Adrian Doe',
-	},
+  {
+    id: 1,
+    review:
+      'Tempus oncu enim pellen tesque este pretium in neque, elit morbi sagittis lorem habi mattis Pellen tesque pretium feugiat vel morbi suspen dise sagittis lorem habi tasse morbi.',
+    rating: 4,
+    createdBy: 'John Doe',
+  },
+  {
+    id: 2,
+    review:
+      'Tempus oncu enim pellen tesque este pretium in neque, elit morbi sagittis lorem habi mattis Pellen tesque pretium feugiat vel morbi suspen dise sagittis lorem habi tasse morbi.',
+    rating: 5,
+    createdBy: 'Emma Doe',
+  },
+  {
+    id: 3,
+    review:
+      'Tempus oncu enim pellen tesque este pretium in neque, elit morbi sagittis lorem habi mattis Pellen tesque pretium feugiat vel morbi suspen dise sagittis lorem habi tasse morbi.',
+    rating: 2,
+    createdBy: 'Adrian Doe',
+  },
 ];
 
 const Home = () => {
-	return (
-		<main>
-			<HomeHero />
-			<div className='xl:px-[15vw]'>
-				<FeatureList />
-				<ProductSection content={mockedContent} heading='mobile products' />
-				<ProductSection content={mockedContent} heading='smart watches' />
-			</div>
-			<Sales heading='new year sale' discount={10} />
-			<div className='xl:px-[15vw]'>
-				<Posts posts={mockedPosts} />
-				<Carousel content={mockedReviews} variant='review' arrows />
-			</div>
-		</main>
-	);
+  return (
+    <main>
+      <HomeHero />
+      <div className='xl:px-[15vw]'>
+        <FeatureList />
+        <ProductSection content={mockedContent} heading='mobile products' />
+        <ProductSection content={mockedContent} heading='smart watches' />
+      </div>
+      <Sales heading='new year sale' discount={10} />
+      <div className='xl:px-[15vw]'>
+        <Posts />
+        <Carousel content={mockedReviews} variant='review' arrows />
+      </div>
+    </main>
+  );
 };
 
 export default Home;
