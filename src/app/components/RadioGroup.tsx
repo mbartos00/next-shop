bf3a8e9398c90c6057e70d296accc9abd7d613e6
mbartos <maciekbartos8@gmail.com>
2023-11-18 11:57:25 +0100

    Add radio group component

---
+++ src/app/components/RadioGroup.tsx
@@ -0,0 +1,39 @@
+import { Field } from 'formik';
+import capitalize from '../utils/capitalize';
+
+type Props = {
+  name: string;
+  values: string[];
+  title: string;
+  checked?: string;
+};
+
+const RadioGroup = ({ name, values, title, checked }: Props) => {
+  return (
+    <div className='w-fit'>
+      <h3 className='mb-4 font-medium uppercase underline' id={name}>
+        {title}
+      </h3>
+      <div className='flex flex-col gap-2' role='group' aria-labelledby={name}>
+        {values.map((value) => (
+          <label
+            className={`font-medium transition-all duration-200 md:cursor-pointer ${
+              checked?.toLowerCase() === value ? 'text-secondary' : 'text-primary'
+            }`}
+            key={value}
+          >
+            <Field
+              className='pointer-events-none fixed opacity-0'
+              type='radio'
+              name={name}
+              value={value}
+            />
+            {capitalize(value)}
+          </label>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RadioGroup;
