cdf598ff5b8dd6d15feb3965b42f165dab54a4ef
mbartos <maciekbartos8@gmail.com>
2023-11-20 19:01:24 +0100

    Add pagination

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -4,11 +4,18 @@ import { NextResponse } from 'next/server';
 import { URL } from 'url';
 
 export const GET = async (req: Request) => {
-  const category = new URL(req.url).searchParams.get('category');
+  const url = new URL(req.url);
+  const category = url.searchParams.get('category');
+  const page = Number(url.searchParams.get('page'));
+  const perPage = Number(url.searchParams.get('perPage'));
+  const productCount = await prisma.product.count();
+
   try {
     let products;
     if (category) {
       products = await prisma.product.findMany({
+        skip: (page - 1) * perPage,
+        take: perPage,
         where: {
           category: {
             name: capitalize(category),
@@ -31,6 +38,8 @@ export const GET = async (req: Request) => {
       });
     } else {
       products = await prisma.product.findMany({
+        skip: (page - 1) * perPage,
+        take: perPage,
         include: {
           category: {
             select: {
@@ -47,12 +56,23 @@ export const GET = async (req: Request) => {
         },
       });
     }
-
     if (!products) {
       return NextResponse.json({ message: 'Products not found' }, { status: 404 });
     }
+    if (page > Math.ceil(productCount / perPage)) {
+      return NextResponse.json({ message: 'Page not found' }, { status: 404 });
+    }
 
-    return NextResponse.json(products);
+    return NextResponse.json(
+      {
+        products,
+        perPage,
+        page,
+        totalPages: Math.ceil(Math.ceil(productCount / perPage)),
+        count: productCount,
+      },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json({ message: 'Get error', error }, { status: 500 });
   }
