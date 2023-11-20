78a816ab532887a5268cbf3cbfb950aa3651c166
mbartos <maciekbartos8@gmail.com>
2023-11-20 19:02:08 +0100

    Add className prop

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -9,9 +9,10 @@ type Props = {
   placeholder?: string;
   required?: boolean;
   as?: string;
+  className?: string;
 };
 
-const Input = ({ as, label, name, placeholder, required }: Props) => {
+const Input = ({ as, label, name, placeholder, required, className = '' }: Props) => {
   const { errors, touched }: FormikProps<ContactFormValues> = useFormikContext();
   const isError =
     errors[name as keyof ContactFormValues] && touched[name as keyof ContactFormValues];
@@ -24,7 +25,7 @@ const Input = ({ as, label, name, placeholder, required }: Props) => {
       )}
       <Field
         as={as}
-        className={`mt-2 border ${
+        className={`mt-2 border ${className} ${
           isError ? 'border-red-700' : 'border-[#D9D9D9]'
         } py-2 pl-4 font-light text-[#3a3a3a] outline-offset-0 outline-primary placeholder:text-sm ${
           as === 'textarea' ? 'resize-none py-6' : ''
