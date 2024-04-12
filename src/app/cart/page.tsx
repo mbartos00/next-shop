61ffbf970a5f0455749a220cb460ca90cfbe9afe
mbartos00 <maciekbartos8@gmail.com>
2024-04-12 14:24:45 +0200

    fix: change .jsx to .tsx, add page title

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,19 +1,24 @@
 'use client';
-import { Suspense } from 'react';
+import { useEffect } from 'react';
 import Hero from '../components/Hero/Hero';
 import useStore from '../hooks/useStore';
 import useCartStore from '../store/cartStore';
 import CartItem from './CartItem';
+import CartSummary from './CartSummary';
 
 const Cart = () => {
   const cart = useStore(useCartStore, (state) => state.cart);
+
+  useEffect(() => {
+    document.title = 'Cart';
+  }, []);
+
   return (
     <main>
       <Hero title='cart' />
       <div className='flex flex-col gap-6 py-7 md:py-14 xl:px-[15vw] xl:py-28'>
-        {cart?.map((product) => (
-          <CartItem key={product.id} product={product} />
-        ))}
+        {cart?.map((product) => <CartItem key={product.id} product={product} />)}
+        <CartSummary />
       </div>
     </main>
   );
