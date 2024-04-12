760bc2222d852588520e23e9811b058aea1c17b1
mbartos00 <maciekbartos8@gmail.com>
2024-04-12 12:31:39 +0200

    fix: fix getting latest products

--- src/app/api/products/latest/route.ts
+++ src/app/api/products/latest/route.ts
@@ -1,17 +1,10 @@
 import prisma from '@/app/utils/prismadb';
-import dayjs from 'dayjs';
+import { Product } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
 export const GET = async () => {
   try {
-    const now = dayjs();
-    const products = await prisma.product.findMany({
-      where: {
-        createdAt: {
-          lte: now.toDate(),
-          gte: now.subtract(1, 'month').toDate(),
-        },
-      },
+    const products: Product[] = await prisma.product.findMany({
       include: {
         category: {
           select: {
@@ -25,12 +18,17 @@ export const GET = async () => {
           },
         },
       },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 5,
     });
-    if (!products) {
+
+    if (!products || products.length === 0) {
       return NextResponse.json({ message: 'Products not found' }, { status: 404 });
     }
 
-    return NextResponse.json(products.slice(0, 5));
+    return NextResponse.json(products);
   } catch (error) {
     return NextResponse.json({ message: 'Get error', error }, { status: 500 });
   }
