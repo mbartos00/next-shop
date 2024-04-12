0d51ccc1560275d33ee2d7162444a88ac95ba286
mbartos00 <maciekbartos8@gmail.com>
2024-04-12 14:24:15 +0200

    fix: fix styles of cart items

--- src/app/cart/CartItem.tsx
+++ src/app/cart/CartItem.tsx
@@ -8,14 +8,15 @@ type Props = {
 
 const CartItem = ({ product }: Props) => {
   const { addToCart, removeProduct, removeFromCart } = useCartStore();
+
   return (
-    <div className='grid w-full grid-cols-1 gap-2 px-8 md:grid-cols-4 md:gap-4 md:gap-y-0 xl:grid-cols-4 xl:border-b-2 xl:border-[#EEEEEE] xl:py-4'>
-      <div className='flex w-full flex-col items-center md:col-span-4 md:flex-row md:gap-4 xl:col-span-2'>
+    <div className='grid w-full grid-cols-1 gap-2 px-8 md:grid-cols-4 md:gap-4 md:gap-y-0 xl:grid-cols-5 xl:border-b-2 xl:border-[#EEEEEE] xl:py-4'>
+      <div className='flex w-full flex-col items-center md:flex-row md:gap-4 xl:col-span-2'>
         <Image
           src={product.images[0].path}
           alt={`${product.name} image`}
           width={500}
-          height={500}
+          height={300}
           style={{ width: '100%' }}
           quality={100}
         />
@@ -24,12 +25,12 @@ const CartItem = ({ product }: Props) => {
           <p className='text-secondary md:text-lg'>${product.price}</p>
         </div>
       </div>
-      <div className='flex w-full justify-center gap-2 md:col-start-3 xl:place-items-center'>
+      <div className='flex w-full justify-center gap-2 md:col-start-3 md:self-center xl:col-start-4 xl:place-items-center'>
         <Button
           tag='button'
           text='-'
           onClick={() => removeProduct(product)}
-          className='border border-[#EEEEEE] bg-transparent px-4 py-1 shadow-xl'
+          className='border border-[#EEEEEE] bg-transparent px-4 py-1 shadow-xl '
         />
         <div className='my-auto w-14 border border-[#EEEEEE] py-1 text-center shadow-xl md:py-2 xl:py-0.5'>
           {product.cartQuantity}
@@ -41,7 +42,7 @@ const CartItem = ({ product }: Props) => {
           className='border border-[#EEEEEE] bg-transparent px-4 py-1 shadow-xl'
         />
       </div>
-      <div className='mt-2 flex items-center justify-center'>
+      <div className='mt-2 flex items-center justify-center md:mt-0'>
         <h2 className='text-2xl text-secondary'>
           ${(product.price * product.cartQuantity).toFixed(2)}
         </h2>
